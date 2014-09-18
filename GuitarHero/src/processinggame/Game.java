934615f81a619c2ece3b085da38724b250dabd24
176148 <176148@HSS-101-20.westwindsor.k12.nj.us>
2014-09-18 13:44:25 -0400

    commit

--- GuitarHero/src/processinggame/Game.java
+++ GuitarHero/src/processinggame/Game.java
@@ -5,15 +5,21 @@ public class Game
 	
 	private GuitarHeroGui gGui;
 	private MenuGui mGui;
+	private int gamestate;
 
 	public static void main(String[] args) 
 	{
 		
 	}
 	
-	public void changeState()
+	public void changeState(int gamestate)
 	{
 		
+	}
+	
+	public int getGameState()
+	{
+		return gamestate;
 	}
 
 }
