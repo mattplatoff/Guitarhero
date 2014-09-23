e917c415831e340b3a7cd8f831d090a9eecacb48
167564 <167564@HSS-101-19.westwindsor.k12.nj.us>
2014-09-23 12:39:25 -0400

    gui methods

--- GuitarHero/src/processinggame/Gui.java
+++ GuitarHero/src/processinggame/Gui.java
@@ -2,5 +2,18 @@ package processinggame;
 
 class Gui 
 {
-
+	public void showBeat(ArrayList<Beat> allBeats)
+	{
+		
+	}
+	
+	public void showNotePlayed(Beat beatPlayedByGuitar)
+	{
+		
+	}
+	
+	public void showScore(int score)
+	{
+		
+	}
 }
