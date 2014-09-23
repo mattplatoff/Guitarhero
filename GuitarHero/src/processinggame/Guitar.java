ab119b51c12cfd220eb9e11b62bc7aacdad381e1
167564 <167564@HSS-101-19.westwindsor.k12.nj.us>
2014-09-23 12:18:39 -0400

    guitar method

--- GuitarHero/src/processinggame/Guitar.java
+++ GuitarHero/src/processinggame/Guitar.java
@@ -2,10 +2,14 @@ package processinggame;
 
 public class Guitar 
 {
+	
+	private final key
+	/**
+	 *  Guitar will listen to the keyboard and create and return a beat object based on the keys pressed
+	 *  and the time they have been pressed.
+	 */
 	public Beat playNote()
 	{
 		
 	}
-	
-	public 
 }
