221e91ddd734de3f24fffc7048540d03722d5a73
akshay <akshay@Akshay-Ubuntu>
2014-09-23 21:22:34 -0400

    more changes - akshay

--- GuitarHero/src/processinggame/Guitar.java
+++ GuitarHero/src/processinggame/Guitar.java
@@ -2,21 +2,31 @@ package processinggame;
 
 public class Guitar 
 {
+	/**
+	 * This method will get the key that has been pressed 
+	 */
+	public void getKeyPressed()
+	{
+		
+	}
 	
-	private final key
 	/**
-	 *  called when the spacebar on the guitar is pressed.  returns an araylist with 
-	 *  what keys were checked so that score keeper can compare it to the beats in the score zone to deternine points.
+	 * OLD CODE
 	 */
-	public Beat playNotes()
-	{
-		
-	}
-	/**
-	 * checks if a given key was pressed
-	 */
-	public void keyPressed()
-	{
-		
-	}
+//	private final key
+//	/**
+//	 *  called when the spacebar on the guitar is pressed.  returns an arraylist with 
+//	 *  what keys were checked so that score keeper can compare it to the beats in the score zone to determine points.
+//	 */
+//	public Beat playNotes()
+//	{
+//		
+//	}
+//	/**
+//	 * checks if a given key was pressed
+//	 */
+//	public void keyPressed()
+//	{
+//		
+//	}
 }
