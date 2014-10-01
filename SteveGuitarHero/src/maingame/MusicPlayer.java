fe920998897418ca1f9e8fdcc0a1af353fbe0166
167564 <167564@HSS-101-19.westwindsor.k12.nj.us>
2014-10-01 13:30:30 -0400

    added music player class

---
+++ SteveGuitarHero/src/maingame/MusicPlayer.java
@@ -0,0 +1,36 @@
+package maingame;
+
+import ddf.minim.*;
+import processing.core.PApplet;
+
+public class MusicPlayer {
+	
+	Minim minim;
+	AudioPlayer player;
+	AudioInput input;
+
+	void setup()
+	{
+	  //let's make the window a little bigger
+	  size(400,400);
+
+	  minim = new Minim(this);
+	  player = minim.loadFile("song.mp3");
+	  input = minim.getLineIn();
+	}
+
+	void draw(){ }
+
+	void mousePressed()
+	{
+	  player.play();
+	}
+
+	void mouseReleased()
+	{
+	  player.close();
+
+	  //since close closes the file, we'll load it again
+	  player = minim.loadFile("song.mp3");
+	}
+}
