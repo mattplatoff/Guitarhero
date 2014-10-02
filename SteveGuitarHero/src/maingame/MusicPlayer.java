3878cc46ec21f3aff85490eb1b3baf2b83862220
akshay <akshay@akshay-linux>
2014-10-02 16:43:14 -0400

    commit

--- SteveGuitarHero/src/maingame/MusicPlayer.java
+++ SteveGuitarHero/src/maingame/MusicPlayer.java
@@ -7,17 +7,14 @@ public class MusicPlayer extends PApplet {
 	
 	Minim minim;
 	AudioPlayer player;
-//	AudioInput input;
 
 	public void setup()
 	{
-	  //let's make the window a little bigger
 	  size(400,400);
 
 	  minim = new Minim(this);
-	  player = minim.loadFile("../../Resourses/test.mp3");
+	  player = minim.loadFile("../../Resources/test.mp3");
 	  player.play();
-//	  input = minim.getLineIn();
 	}
 
 	public void draw()
@@ -40,14 +37,11 @@ public class MusicPlayer extends PApplet {
 
 //	public void mousePressed()
 //	{
-//	  player.play();
+//	  player.pause();
 //	}
 //
 //	public void mouseReleased()
 //	{
-//	  player.close();
-//
-//	  //since close closes the file, we'll load it again
-//	  player = minim.loadFile("test.mp3");
+//	  player.pause();
 //	}
 }
